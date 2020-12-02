b959f2cc2fcc43fbf3d5de73eae8c2edf0bdfdd7
NemoCZ <seraphcz@gmail.com>
2020-12-02 12:27:39 +0100

    Registracni formular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -12,20 +12,13 @@ import { AccountService } from './_services/account.service';
 export class AppComponent implements OnInit {
   title = 'The Dating app';
   users: any;
-  constructor(private http: HttpClient, private accountService: AccountService){}
+  constructor(private accountService: AccountService){}
   setCurrentUser(): void{
     const user: User = JSON.parse(localStorage.getItem('user')as any);
     this.accountService.setCurrentUser(user);
   }
-  getUsers(): void{
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, err => {
-      console.log(err);
-    });
-  }
+
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 }
